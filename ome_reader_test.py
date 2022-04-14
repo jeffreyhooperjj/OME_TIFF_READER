8a6e5553758aaf234cbe7345ce76a49f0fa9aa11
Jeffrey Hooper <jeffreyrepooh@gmail.com>
2022-04-14 14:09:40 -0700

    Add files via upload

---
+++ ome_reader_test.py
@@ -0,0 +1,21 @@
+import javabridge
+import bioformats
+import re
+
+javabridge.start_vm(class_path=bioformats.JARS)
+
+path = "./3377A8.ome.tif"
+
+# with bioformats.ImageReader(path) as reader:
+# 	print(reader.read())
+
+xml = bioformats.get_omexml_metadata(path)
+
+# (?# matches = re.search(r'Channel Color="" ID="Channel:0:0" Name="DNA (DAPI)"(.+?)"', xml))
+matches = re.findall(r'Name="(.+?)"', xml)
+for match in matches:
+	print(match)
+
+print(xml)
+
+javabridge.kill_vm()
